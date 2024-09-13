52ab87dd8e963a89074b8fc8f153e27cfb414dbf
Kai He <hekai_aggie2013@163.com>
2024-09-13 09:40:52 +0800

    add skeleton state

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+    return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 1, 44, 79),
+        ),
+        body: Placeholder(),
       ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var imgIdx = 1;
+
+  void changeItUp () {
+    setState(() {
+      imgIdx = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:[
+        
+      ],
     );
+  }
+}
